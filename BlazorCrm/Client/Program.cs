74d7263470054dbe23636c6df5b685e5a76d7b82
giannis genniadys <giannis.genniadys@gmail.com>
2023-12-08 14:25:35 +0200

    Created Menu AppBar , Created class for pages , Created Pages and changes css

--- BlazorCrm/Client/Program.cs
+++ BlazorCrm/Client/Program.cs
@@ -1,10 +1,15 @@
 using BlazorCrm.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+//Register Syncfusion license 23.2.4 blazorcrm.client na allajv thn version an den doulevei
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjk2ODI0OUAzMjMzMmUzMDJlMzBRaklkd1RkbkM0cXVkUmpZUnZVeGQvcVVMSitLc3BSU1B5cG9MT1plWE1VPQ==");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddHttpClient("BlazorCrm.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
---
+++ BlazorCrm/Shared/Employees.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.Blazor.Diagram;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlazorCrm.Shared
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        [Required]
+        public string NickName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public DateTime? DateCreated { get; set;}
+        public DateTime? DateDeleted { get; set; }
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+        [JsonIgnore]
+        public List<Note> Notes { get; set; } = new List<Note>();
+
+
+
+    }
+}
