a373beed2017df938eab69316f61d549a21a7d8d
giannis genniadys <giannis.genniadys@gmail.com>
2023-12-20 10:56:25 +0200

    MINOR CHANGES + NEW LICENCE SYNCFUSION

--- BlazorCrm/Client/Program.cs
+++ BlazorCrm/Client/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 
 //Register Syncfusion license 23.2.4 blazorcrm.client na allajv thn version an den doulevei
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjk2ODI0OUAzMjMzMmUzMDJlMzBRaklkd1RkbkM0cXVkUmpZUnZVeGQvcVVMSitLc3BSU1B5cG9MT1plWE1VPQ==");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjk4ODQ1M0AzMjM0MmUzMDJlMzBCK1JSc3l0eHlxUUdyTzVIMWJudFpZYy8vS3RGNllUVjRRVXpRaENRckpRPQ==");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
